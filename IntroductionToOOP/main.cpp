c210fe635d81d433d5a33897fa2894c9b16629ee
ilyav <ilyav@ILIA>
2025-06-30 21:39:25 +0500

    Distance DONE

--- IntroductionToOOP/main.cpp
+++ IntroductionToOOP/main.cpp
@@ -1,5 +1,9 @@
 #include<iostream>
 using namespace std;
+using std::cin;
+using std::cout;
+using std::endl;
+
 
 class Point
 {
@@ -22,7 +26,25 @@ public:
 	{
 		this->y = y;
 	}
+
+	double distance(Point other)
+	{
+		double x_distance = this->x - other.x;
+		double y_distance = this->y - other.y;
+		double distance = sqrt(x_distance*x_distance+y_distance*y_distance);//квадратный корень
+		return distance;
+	}
+
 };
+
+double distance(Point a, Point b)
+{
+	double x_distance = a.get_x() - b.get_x();
+	double y_distance = a.get_y() - b.get_y();
+	double distance = sqrt(x_distance*x_distance+y_distance*y_distance);
+	return distance;
+}
+	
 
 //#define STRUCT_POINT
 
@@ -46,4 +68,14 @@ void main()
 	A.set_y(3);
 	cout << A.get_x() << "\t" << A.get_y() << endl;
 
+	Point B;
+	B.set_x(7);
+	B.set_y(8);
+	cout << B.get_x() << "\t" << B.get_y() << endl;
+
+	cout << "Расстояние от точки А до точки В: " << A.distance(B) << endl;
+	cout << "Расстояние от точки B до точки A: " << B.distance(A) << endl;
+	cout << "Расстояние между точками А и В: " << distance(A,B) << endl;
+	cout << "Расстояние между точками B и A: " << distance(B,A) << endl;
+	
 }
